cfe21a0177992f3de057b366c192365aedc29587
Diego Gonzalez <90977032+DiegoNGonzalez@users.noreply.github.com>
2022-11-21 12:39:07 -0300

    fix bug protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
-export function ProtectedRouted({ children }) {
+function ProtectedRoutes({ children }) {
     const { user, loading } = useAuth();
 
     if (loading) return <h1>Loading</h1>;
     if (!user) return <Navigate to='/login' />;
     return <>{children}</>;
 }
+export default ProtectedRoutes
